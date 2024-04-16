f421ba4a7c355529b53d7490655e4ca1c14e44f1
Matthias Wuyts 6IT <matthiasw609@lln.campussintursula.be>
2024-04-16 17:16:56 +0200

    added map

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -1,21 +1,24 @@
 "use client";
-import React, { useMemo } from "react";
-import dynamic from "next/dynamic";
-
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import styles from './map.module.css';
 export default function MyPage() {
-  const Map = useMemo(
-    () =>
-      dynamic(() => import("@/components/map"), {
-        loading: () => <p>A map is loading</p>,
-        ssr: false,
-      }),
-    []
-  );
+
 
   return (
     <div className="bg-green-200 h-screen">
       <h1 className="text-4xl font-bold text-center p-4">Map Component</h1>
-      <Map />
+      <MapContainer className={styles.map} center={[48.8566, 2.3522]} zoom={10} scrollWheelZoom={true}>
+      <TileLayer
+        attribution="Google Maps"
+        //  // regular
+        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+        url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // terrain
+        maxZoom={20}
+        subdomains={["mt0", "mt1", "mt2", "mt3"]}
+        
+      />
+      </MapContainer>
     </div>
   );
 }
